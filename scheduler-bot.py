b973d67e73828a3332b6310e723ecaa2c0e5a7c9
Alexandra Diem <alexandra.diem@gmail.com>
2021-05-13 19:45:57 +0200

    remove scheduling -- will be done by Google Cloud

--- scheduler-bot.py
+++ scheduler-bot.py
@@ -11,6 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 CHANNEL = os.getenv('SCHEDULER_CHANNEL')
 
+# Define poll
 POLL_TITLE = "Which days can you captain next week?"
 POLL_EMOJIS = ['1️⃣', '2️⃣', '3️⃣']
 POLL_OPTIONS = [
@@ -20,28 +21,33 @@ POLL_OPTIONS = [
     ]
 POLL_MESSAGE = POLL_TITLE + "\n\n" + "\n".join(POLL_OPTIONS)
 
-
+# Instantiate bot
 bot = commands.Bot(command_prefix='!')
 
-@bot.event
-async def on_ready():
-    await bot.wait_until_ready()
-    print("Logged in as:")
-    print(bot.user.name)
-    print("------")
-    channel = bot.get_channel(int(CHANNEL))
-    print("Channel is:")
-    print(channel) #Prints None
-    poll.start(channel)
-
-
-@tasks.loop(seconds=5.0, count=1)
-async def poll(channel):
+async def send_poll(channel):
+    """Send poll message to channel and add poll reactions 
+    """
     await bot.wait_until_ready()
     m = await channel.send(POLL_MESSAGE)
 
     for emoji in POLL_EMOJIS:
         await m.add_reaction(emoji=emoji)
+
+@bot.event
+async def on_ready():
+    """Set up variables and logging
+    """
+    await bot.wait_until_ready()
+
+    print("Logged in as:")
+    print(bot.user.name)
+    print("------")
+
+    channel = bot.get_channel(int(CHANNEL))
+    print("Channel is:")
+    print(channel)
+
+    await send_poll(channel)
 
 
 bot.run(TOKEN)
