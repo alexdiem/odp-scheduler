d5f9cea4338bc75ed56961632fe00fd00f62f5e5
Alexandra Diem <alexandra.diem@gmail.com>
2021-05-13 19:56:56 +0200

    add logging

--- scheduler-bot.py
+++ scheduler-bot.py
@@ -5,6 +5,9 @@ import discord
 from discord.ext import commands, tasks
 from discord.utils import get
 
+import logger
+log = logger.setup_applevel_logger(file_name = 'app_debug.log')
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -27,27 +30,31 @@ bot = commands.Bot(command_prefix='!')
 async def send_poll(channel):
     """Send poll message to channel and add poll reactions 
     """
+    log.debug('Running {}.'.format(send_poll.__name__))
     await bot.wait_until_ready()
+
+    log.debug('Send message to channel: \n{}'.format(POLL_MESSAGE))
     m = await channel.send(POLL_MESSAGE)
 
     for emoji in POLL_EMOJIS:
+        log.debug('Add reaction to poll: {}'.format(emoji))
         await m.add_reaction(emoji=emoji)
 
 @bot.event
 async def on_ready():
     """Set up variables and logging
     """
+    log.debug('Running {}'.format(on_ready.__name__))
     await bot.wait_until_ready()
 
-    print("Logged in as:")
-    print(bot.user.name)
-    print("------")
+    log.debug('Logged in as {}'.format(bot.user.name))
 
     channel = bot.get_channel(int(CHANNEL))
-    print("Channel is:")
-    print(channel)
+    log.debug('Channel is {}'.format(channel))
 
+    log.debug('Calling {} on channel.'.format(send_poll.__name__))
     await send_poll(channel)
 
 
+log.debug('Starting bot.')
 bot.run(TOKEN)
