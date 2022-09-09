cc72171a1f521352947e9748b1cf8ae18b912cd4
alexdiem <alexandra.diem@gmail.com>
2022-09-09 16:41:50 +0200

    add deployment for scheduler bot

--- main.py
+++ main.py
@@ -6,24 +6,34 @@ from dotenv import load_dotenv
 from google.cloud import logging
 
 from odp_scheduler.poll_bot import PollBot
+from odp_scheduler.scheduler_bot import SchedulerBot
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-logging_client = logging.Client()
-log_name = "poll-bot-log"
-logger = logging_client.logger(log_name)
 
-
-@functions_framework.http
-def run_poll_bot(request):  
+def run_bot(BotType, log_name):
     DEBUG = False
+    logging_client = logging.Client()
+    logger = logging_client.logger(log_name)
     if '--debug' in sys.argv:
         DEBUG = True
     logger.log_text('Debug mode: {}'.format(DEBUG), severity="DEBUG")
 
     logger.log_text('Starting poll bot.', severity="DEBUG")
-    bot = PollBot(command_prefix='!', self_bot=False, options='POLL_OPTIONS_SUMMER', log=logger, debug=DEBUG)
+    bot = BotType(command_prefix='!', self_bot=False, options='POLL_OPTIONS_SUMMER', log=logger, debug=DEBUG)
     bot.run(TOKEN)
     return "Success", 200
+
+
+@functions_framework.http
+def run_poll_bot(request):  
+    log_name = "poll-bot-log"
+    return run_bot(PollBot, log_name)
+
+
+@functions_framework.http
+def run_scheduler_bot(request):
+    log_name = "scheduler-bot-log"
+    return run_bot(SchedulerBot, log_name)
--- odp_scheduler/scheduler_bot.py
+++ odp_scheduler/scheduler_bot.py
@@ -1,239 +1,219 @@
-import json
-import logger
-import os
-import random
-
-from datetime import date
-from dotenv import load_dotenv
-from sys import exit
-
 import discord
 from discord.ext import commands
 
-from utils import make_arguments_parser, parse_command_line_arguments
+import json
+import os
+import random
 
 
-log = logger.setup_applevel_logger(file_name = 'app_debug.log')
+class SchedulerBot(commands.Bot):
 
-# Parse command line arguments
-parser = make_arguments_parser()
-args = parse_command_line_arguments(parser, log)
-DEBUG = args.debug
-log.debug('Debug mode: %s', DEBUG)
+    def __init__(self, command_prefix, self_bot, options, log, debug=False):
+        self.DEBUG = debug
+        self.LOG = log
+        self.CHANNEL = os.getenv('SCHEDULER_CHANNEL')
+        self.SCHEDULER_MSG = "@channel I'm a level 1 naive scheduling bot, and I make mistakes. " +\
+                "<@!766548029116907570> will help me fix it.\n"
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-CHANNEL = os.getenv('SCHEDULER_CHANNEL')
-CAPTAINS = os.getenv('CAPTAINS')
-
-with open('POLL_OPTIONS_SUMMER', 'r') as f:
-    POLL_OPTIONS = eval(f.read())
-
-RIDES = list(POLL_OPTIONS.values())
-SCHEDULE = dict((ride, []) for ride in RIDES)
-CAPTAINS_PER_RIDE = zip(RIDES, [1, 1, 2, 1, 1])
-
-SCHEDULER_MSG = "@channel I'm a level 1 naive scheduling bot, and I make mistakes. " +\
-    "<@!766548029116907570> will help me fix it.\n"
-
-# Instantiate bot
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-async def manage_schedule(channel, msg_id):
-    """Read reactions on last poll message 
-    """
-    log.debug('Running {}.'.format(manage_schedule.__name__))
-    await bot.wait_until_ready()
-
-    try:
-        msg = await channel.fetch_message(msg_id)
-    except discord.errors.NotFound:
-        log.error("Discord error: Message ID {} not found.".format(msg_id))
-        await bot.close()
-        exit()
+        with open(options, 'r') as f:
+            self.POLL_OPTIONS = eval(f.read())
         
-    log.debug("Got message with ID {}".format(msg_id))
-        
-    reactions = msg.reactions
+        self.RIDES = list(self.POLL_OPTIONS.values())
+        self.SCHEDULE = dict((ride, []) for ride in self.RIDES)
+        self.CAPTAINS_PER_RIDE = zip(self.RIDES, [1, 1, 2, 1, 1])
 
-    log.debug('Calling {} on channel.'.format(read_reactions.__name__))
-    avail = await read_reactions(reactions)
-
-    for ride, n_cap in CAPTAINS_PER_RIDE:
-        log.debug('Calling {} on channel for {}.'.
-            format(create_ride_schedule.__name__, ride))
-        captains = await create_ride_schedule(ride, n_cap, avail)
-        SCHEDULE[ride] = captains
+        # Instantiate bot
+        intents = discord.Intents.default()
+        intents.message_content = True
+        commands.Bot.__init__(self, command_prefix=command_prefix, intents=intents)
 
 
-    log.debug('Calling {}.'.format(update_logs.__name__))
-    update_logs()
-    
-    if not DEBUG:
-        log.debug('Calling {} on channel.'.format(post_schedule.__name__))
-        await post_schedule(channel)
-
-
-async def read_reactions(reactions):
-    """Read reactions from scheduler poll
-    """
-    log.debug('Running {}.'.format(read_reactions.__name__))
-    await bot.wait_until_ready()
-
-    log.debug('Getting availability from poll.')
-    emojis = list(POLL_OPTIONS.keys())
-    avail = dict((ride, []) for ride in RIDES)
-    for reaction in reactions:
-        ride_index = ''
-        try:
-            ride_index = emojis.index(reaction.emoji)
-        except ValueError:
-            log.error("Invalid reaction found: " + reaction.emoji)
-            continue
-
-        users = [user async for user in reaction.users()]
-        for user in users:
-            if not user.bot:
-                avail[RIDES[ride_index]].append(user)
-    
-    log.debug('Availability is: {}'.format(
-        "\n".join(f'{k}: {users_to_names(v)}' for k,v in avail.items())
-    ))
-    return avail
-
-
-async def create_ride_schedule(ride, n_cap, avail):
-    """Create road captain schedule
-    """
-    log.debug('Running {}.'.format(create_ride_schedule.__name__))
-    await bot.wait_until_ready()
-
-    log.debug('Choosing road captains for {}'.format(ride))
-    captains = []
-    
-    # choose randomly for now
-    for i in range(n_cap):
-
-        #captain = prioritise_absence(ride, avail)
+    async def manage_schedule(self, channel, msg_id):
+        """Read reactions on last poll message 
+        """
+        self.LOG.debug('Running {}.'.format(self.manage_schedule.__name__))
+        await self.wait_until_ready()
 
         try:
-            captain = random.choice(avail[ride])
-        except IndexError:
-            captain = None
+            msg = await channel.fetch_message(msg_id)
+        except discord.errors.NotFound:
+            self.LOG.error("Discord error: Message ID {} not found.".format(msg_id))
+            await self.close()
+            exit()
+            
+        self.LOG.debug("Got message with ID {}".format(msg_id))
+            
+        reactions = msg.reactions
 
-        captains.append(captain)
+        self.LOG.debug('Calling {} on channel.'.format(self.read_reactions.__name__))
+        avail = await self.read_reactions(reactions)
 
-        # don't pick same captain twice for one ride
-        if captain in avail[ride]:
-            avail[ride].remove(captain)
-
-        for s in avail.keys():
-            if captain in avail[s] and len(avail[s]) > 2:
-                log.debug('Scheduled {} on {}. Removing them from {}'.
-                    format(captain.display_name, ride, s))
-                avail[s].remove(captain)
-
-    log.debug(
-        "Road captains for {} are {}".format(ride, users_to_names(captains))
-    )
-    return captains
+        for ride, n_cap in self.CAPTAINS_PER_RIDE:
+            self.LOG.debug('Calling {} on channel for {}.'.
+                format(self.create_ride_schedule.__name__, ride))
+            captains = await self.create_ride_schedule(ride, n_cap, avail)
+            self.SCHEDULE[ride] = captains
 
 
-def prioritise_absence(ride, avail):
-    """Prioritise captains that have been absent longer than others. Not yet in use.
-    """
-    log.debug('Running {}.'.format(prioritise_absence.__name__))
-
-
-    with open('schedule') as f:
-        schedule = [json.loads(line) for line in f]
-    
-    print(schedule)
-
-
-def update_logs():
-    """Update log files.
-    """
-    log.debug('Running {}.'.format(update_logs.__name__))
-
-    # log schedule to file
-    log.debug('Saving schedule to log.')
-
-    printable_schedule = SCHEDULE.copy()
-    for ride in printable_schedule:
-        printable_schedule[ride] = users_to_names(printable_schedule[ride])
-    printable_schedule['date'] = str(date.today())
-
-    json_schedule = json.dumps(printable_schedule)
-    with open("schedule", 'a') as f:
-        f.write('\n' + json_schedule)
-    log.debug(json_schedule)
-
-
-async def post_schedule(channel):
-    """Post schedule to channel.
-    """
-    log.debug('Running {}.'.format(create_ride_schedule.__name__))
-    await bot.wait_until_ready()
-
-    msg = SCHEDULER_MSG + "\nRoad captains for this week are"
-
-    schedule_post = SCHEDULE.copy()
-    for ride in schedule_post:
-        schedule_post[ride] = users_to_tags(schedule_post[ride])
-
-    for k, v in schedule_post.items():
-        msg += f"\n\n**{k}**\n" +\
-                "\n".join(f'{c}' for c in v)
-
-    log.debug('Send message to channel: \n{}'.format(msg))
-    m = await channel.send(msg)
-
-
-def users_to_names(users):
-    """Convert a list of Users to a list of user names (str).
-    """
-    return [u.display_name if u is not None else '' for u in users]
-
-
-def users_to_tags(users):
-    """Convert a list of Users to a list of tags (str).
-    """
-    return ["<@!{}>".format(u.id) if u is not None else '' for u in users]
-
-
-def user_to_tag(user):
-    """Convert a list of Users to a list of tags (str).
-    """
-    return "<@!{}>".format(user.id) if user is not None else ''
-
-
-@bot.event
-async def on_ready():
-    """Set up variables and logging
-    """
-    log.debug('Running {}'.format(on_ready.__name__))
-    await bot.wait_until_ready()
-
-    log.debug('Logged in as {}'.format(bot.user.name))
-
-    channel = bot.get_channel(int(CHANNEL))
-    log.debug('Channel is {}'.format(channel))
-
-    with open("MESSAGE_ID", 'r') as f:
-        msg_id = int(f.readline())
-    log.debug('Read poll message with ID {}.'.format(msg_id))
-
-    log.debug('Calling {} on channel.'.format(manage_schedule.__name__))
-    await manage_schedule(channel, msg_id)
-
-    log.debug('Shutdown poll bot.')
-    await bot.close()
-
+        self.LOG.debug('Calling {}.'.format(self.update_logs.__name__))
+        self.update_logs()
         
-if __name__ == "__main__":
-    log.debug('Starting scheduler bot.')
-    bot.run(TOKEN)
+        if not self.DEBUG:
+            self.LOG.debug('Calling {} on channel.'.format(self.post_schedule.__name__))
+            await self.post_schedule(channel)
+
+
+    async def read_reactions(self, reactions):
+        """Read reactions from scheduler poll
+        """
+        self.LOG.debug('Running {}.'.format(self.read_reactions.__name__))
+        await self.wait_until_ready()
+
+        self.LOG.debug('Getting availability from poll.')
+        emojis = list(self.POLL_OPTIONS.keys())
+        avail = dict((ride, []) for ride in self.RIDES)
+        for reaction in reactions:
+            ride_index = ''
+            try:
+                ride_index = emojis.index(reaction.emoji)
+            except ValueError:
+                self.LOG.error("Invalid reaction found: " + reaction.emoji)
+                continue
+
+            users = [user async for user in reaction.users()]
+            for user in users:
+                if not user.bot:
+                    avail[self.RIDES[ride_index]].append(user)
+        
+        self.LOG.debug('Availability is: {}'.format(
+            "\n".join(f'{k}: {self.users_to_names(v)}' for k,v in avail.items())
+        ))
+        return avail
+
+
+    async def create_ride_schedule(self, ride, n_cap, avail):
+        """Create road captain schedule
+        """
+        self.LOG.debug('Running {}.'.format(self.create_ride_schedule.__name__))
+        await self.wait_until_ready()
+
+        self.LOG.debug('Choosing road captains for {}'.format(ride))
+        captains = []
+        
+        # choose randomly for now
+        for i in range(n_cap):
+
+            #captain = prioritise_absence(ride, avail)
+
+            try:
+                captain = random.choice(avail[ride])
+            except IndexError:
+                captain = None
+
+            captains.append(captain)
+
+            # don't pick same captain twice for one ride
+            if captain in avail[ride]:
+                avail[ride].remove(captain)
+
+            for s in avail.keys():
+                if captain in avail[s] and len(avail[s]) > 2:
+                    self.LOG.debug('Scheduled {} on {}. Removing them from {}'.
+                        format(captain.display_name, ride, s))
+                    avail[s].remove(captain)
+
+        self.LOG.debug(
+            "Road captains for {} are {}".format(ride, self.users_to_names(captains))
+        )
+        return captains
+
+
+    def prioritise_absence(self, ride, avail):
+        """Prioritise captains that have been absent longer than others. Not yet in use.
+        """
+        self.LOG.debug('Running {}.'.format(self.prioritise_absence.__name__))
+
+
+        with open('schedule') as f:
+            schedule = [json.loads(line) for line in f]
+        
+        print(schedule)
+
+
+    def update_logs(self):
+        """Update log files.
+        """
+        self.LOG.debug('Running {}.'.format(self.update_logs.__name__))
+
+        # log schedule to file
+        self.LOG.debug('Saving schedule to log.')
+
+        printable_schedule = self.SCHEDULE.copy()
+        for ride in printable_schedule:
+            printable_schedule[ride] = self.users_to_names(printable_schedule[ride])
+        printable_schedule['date'] = str(self.date.today())
+
+        json_schedule = json.dumps(printable_schedule)
+        with open("schedule", 'a') as f:
+            f.write('\n' + json_schedule)
+        self.LOG.debug(json_schedule)
+
+
+    async def post_schedule(self, channel):
+        """Post schedule to channel.
+        """
+        self.LOG.debug('Running {}.'.format(self.create_ride_schedule.__name__))
+        await self.LOG.wait_until_ready()
+
+        msg = self.SCHEDULER_MSG + "\nRoad captains for this week are"
+
+        schedule_post = self.SCHEDULE.copy()
+        for ride in schedule_post:
+            schedule_post[ride] = self.users_to_tags(schedule_post[ride])
+
+        for k, v in schedule_post.items():
+            msg += f"\n\n**{k}**\n" +\
+                    "\n".join(f'{c}' for c in v)
+
+        self.LOG.debug('Send message to channel: \n{}'.format(msg))
+        m = await channel.send(msg)
+
+
+    def users_to_names(self, users):
+        """Convert a list of Users to a list of user names (str).
+        """
+        return [u.display_name if u is not None else '' for u in users]
+
+
+    def users_to_tags(self, users):
+        """Convert a list of Users to a list of tags (str).
+        """
+        return ["<@!{}>".format(u.id) if u is not None else '' for u in users]
+
+
+    def user_to_tag(self, user):
+        """Convert a list of Users to a list of tags (str).
+        """
+        return "<@!{}>".format(user.id) if user is not None else ''
+
+
+    async def on_ready(self):
+        """Set up variables and logging
+        """
+        self.LOG.debug('Running {}'.format(self.on_ready.__name__))
+        await self.wait_until_ready()
+
+        self.LOG.debug('Logged in as {}'.format(self.user.name))
+
+        channel = self.get_channel(int(self.CHANNEL))
+        self.LOG.debug('Channel is {}'.format(channel))
+
+        with open("MESSAGE_ID", 'r') as f:
+            msg_id = int(f.readline())
+        self.LOG.debug('Read poll message with ID {}.'.format(msg_id))
+
+        self.LOG.debug('Calling {} on channel.'.format(self.manage_schedule.__name__))
+        await self.manage_schedule(channel, msg_id)
+
+        self.LOG.debug('Shutdown poll bot.')
+        await self.close()
